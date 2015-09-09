8cd7dc137dae65eb2e06b07c9a05c0550c74f38a
Patrick Cremin <pwcremin@gmail.com>
2015-09-09 14:35:39 -0500

    dropped /static

--- server.py
+++ server.py
@@ -5,8 +5,6 @@ import json
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('VCAP_APP_PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
 
 app = Flask(__name__)
 
